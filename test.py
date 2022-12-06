f4154f992d12cd946d4388b3c83ab117401947c2
hannahrfowler <hfowler@oberlin.edu>
2022-12-06 16:21:01 -0500

    Trying to fix, still no

--- test.py
+++ test.py
@@ -21,19 +21,19 @@ TRAINING_FILE_PATH = SEQUENCE_EXAMPLES + '/training_poly_tracks.tfrecord/'
 
 FINAL_OUTPUT_DIR = path + '/tmp/polyphony_rnn/'
 
-os.system("convert_dir_to_note_sequences --input_dir=\"" + INPUT_DIRECTORY + "\" --output_file=\"" + SEQUENCES_TFRECORD + "\" --recursive")
+#os.system("convert_dir_to_note_sequences --input_dir=\"" + INPUT_DIRECTORY + "\" --output_file=\"" + SEQUENCES_TFRECORD + "\" --recursive")
 
 #Trains a .mag
-os.system("polyphony_rnn_create_dataset --input=\"" + SEQUENCES_TFRECORD + "\" --output_dir=\"" + SEQUENCE_EXAMPLES + "\" --eval_ratio=0.10")
+#os.system("polyphony_rnn_create_dataset --input=\"" + SEQUENCES_TFRECORD + "\" --output_dir=\"" + SEQUENCE_EXAMPLES + "\" --eval_ratio=0.10")
 
 
 # 2) Training a model / generating a .mag file
 # this code fails, with a very long error, and generates some graphs and stuff, but fails to generate a mag file and crashes the compiler
-os.system("polyphony_rnn_train --run_dir=\"" + RUNNING_DIRECTORY + "\" --sequence_example_file=\"" + TRAINING_FILE_PATH + "\" --hparams=batch_size=64,rnn_layer_sizes=[64,64]\" --num_training_steps=20000")
+os.system("polyphony_rnn_train --run_dir=\"/tmp/polyphony_rnn/logdir/run1\" --sequence_example_file=\"/tmp/polyphony_rnn/sequence_examples/training_poly_tracks.tfrecord\" --hparams=\"batch_size=64,rnn_layer_sizes=[64,64]\" --num_training_steps=20000")
 
 
 
 # 3) Generating midi from model
 #Runs from a .mag file
 BUNDLE_PATH = path + '/polyphony_rnn.mag'
-os.system("polyphony_rnn_generate --bundle_file=\"" + BUNDLE_PATH +  "\" --output_dir=\"" + FINAL_OUTPUT_DIR + "\" --primer_pitches=\"[67, 64, 60]\"")
+#os.system("polyphony_rnn_generate --bundle_file=\"" + BUNDLE_PATH +  "\" --output_dir=\"" + FINAL_OUTPUT_DIR + "\" --primer_pitches=\"[67, 64, 60]\"")

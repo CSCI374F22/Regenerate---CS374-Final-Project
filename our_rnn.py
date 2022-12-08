e5e47407337d0a430a1e32e1426cbeeafc793123
hannahrfowler <hfowler@oberlin.edu>
2022-12-07 21:48:28 -0500

    Workin on it

--- our_rnn.py
+++ our_rnn.py
@@ -12,6 +12,8 @@ from note_seq import midi_io
 import os
 from note_seq import sequences_lib # https://github.com/magenta/note-seq/blob/a7ea6b3ce073b0791fc5e89260eae8e621a3ba0c/note_seq/chord_inference.py for quantization of note_seq
 from note_seq import chords_encoder_decoder
+import numpy as np
+import pandas as pd
 
 def prepreocessing():
     tf.logging.set_verbosity(tf.logging.INFO)
@@ -34,6 +36,7 @@ def prepreocessing():
         # need all root, dirs, files in order to get the files alone
         # loop though all the files
         filename_chord_list = []
+        all_notes = []
         for filename in files:
             #print(filename)
             if len(dirs) == 0:
@@ -41,62 +44,46 @@ def prepreocessing():
                 
                 # convert midi file to note seq
                 note_sequence = midi_io.midi_file_to_note_sequence(filepath)
+                #print(note_sequence.notes[0].pitch)
+                notes = extract_notes(note_sequence)
+                """ for key in notes:
+                    if key == 'pitch':
+                        #print(notes[key])
+                print('----------------------') """
 
+                # getting large collection of all the notes in all files
+                all_notes.append(notes)
 
-                # trim note sequence
-                subsequence = sequences_lib.trim_note_sequence(note_sequence, 0, 60)
+                # gets the transpose of the array, giving us full list of all pitches, steps, and durations
+                #train_notes = np.stack([all_notes[all_notes.index(key)] for key in key_order], axis=1)
+                
+                # translate list of notes to be formatted as pitch, step, duration
+                """ train_notes = []
+                for note in all_notes:
+                    L = [note['pitch'], note['step'], note['duration']]
+                    train_notes.append(L)
+                
+                print("dimensions: ", np.shape(train_notes)) """
 
-                # referenced https://github.com/magenta/note-seq/blob/a7ea6b3ce073b0791fc5e89260eae8e621a3ba0c/note_seq/sequences_lib.py#L988
-                """Quantize a NoteSequence proto relative to tempo.
-                The input NoteSequence is copied and quantization-related fields are
-                populated. Sets the `steps_per_quarter` field in the `quantization_info`
-                message in the NoteSequence.
-                Note start and end times, and chord times are snapped to a nearby quantized
-                step, and the resulting times are stored in a separate field (e.g.,
-                quantized_start_step). See the comments above `QUANTIZE_CUTOFF` for details on
-                how the quantizing algorithm works.
-                Args:
-                    note_sequence: A music_pb2.NoteSequence protocol buffer.
-                    steps_per_quarter: Each quarter note of music will be divided into this many
-                    quantized time steps."""
+                #notes_ds = tf.data.Dataset.from_tensor_slices(train_notes)
+                #notes_ds.element_spec
 
-                # quantize note sequence
-                quantized_sequence = sequences_lib.quantize_note_sequence(
-                subsequence, steps_per_quarter=4)
-                # infer chords
-                chord_inference.infer_chords_for_sequence(
-                    quantized_sequence)
-                #print(note_sequence)
+                # copied from https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/audio/music_generation.ipynb#scrollTo=mvNHCHZdXG2P
+                # TODO: below is all copy pasted
 
-                # get all chords in quantized ntoe sequence
-                chords = [(ta.text, ta.time) for ta in quantized_sequence.text_annotations] 
-                #for ta in quantized_sequence.text_annotations:    
-                    #print(ta.text, ta.time)
+                all_notes = pd.concat(all_notes)
 
-                # get encodings of all chords in note sequence
-                arr = []
+                n_notes = len(all_notes)
+                print('Number of notes parsed:', n_notes)
 
-                # define encoder
-                enc = chords_encoder_decoder.PitchChordsEncoderDecoder()
+                key_order = ['pitch', 'step', 'duration']
+                train_notes = np.stack([all_notes[key] for key in key_order], axis=1)
 
-                for chord in chords:
-                    # get chord name
-                    chord_name = chord[0]
-                    # encoding array
-
-                    # depending on how we're using the one hot encoding it might be better to not use this
-                    # way since it makes things really long
-                    encoding = enc.events_to_input([chord_name], 0)
-                    formatted_encoding = chord_formatting(encoding)
-                    #print(formatted_encoding)
-                    arr.append(formatted_encoding)
-                    #print("array now: ", arr)
-
-                    chord_dict[chord_name] = formatted_encoding
-                    filename_chord_list.append(chord_name)
+                notes_ds = tf.data.Dataset.from_tensor_slices(train_notes)
+                notes_ds.element_spec
                 
 
-                file_name_to_seq_encoding[filepath] = filename_chord_list
+                
 
     #print(file_name_to_seq_encoding) 
 
@@ -126,6 +113,48 @@ def chord_formatting(arr):
     return res
 
 
+# referenced https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/audio/music_generation.ipynb
+def extract_notes(note_sequence):
+    notes = dict()
+    #print(note_sequence.notes[0].pitch)
+    # midi pitch
+
+    # sorted note_sequences by start_time
+    sorted_note_seq = sorted(note_sequence.notes, key=lambda x: x.start_time)
+    #print(sorted_note_seq)
+    
+    # set prev_start to be first value
+    prev_start = sorted_note_seq[0].start_time
+
+    # loop through the notes in note_sequence
+    for note in sorted_note_seq:
+        note_name = note.pitch
+        # start time
+        start = note.start_time
+        end = note.end_time
+
+        duration = end - start
+
+        step = start - prev_start
+
+        prev_start = start
+
+        add_to_dict(notes, 'pitch', note_name)
+        add_to_dict(notes, 'start', start)
+        add_to_dict(notes, 'end', end)
+        add_to_dict(notes, 'step', step)
+        add_to_dict(notes, 'duration', duration)
+    
+    # return notes dictionary (as pd frame)
+    return pd.DataFrame({name: np.array(value) for name, value in notes.items()}) # copied
+
+def add_to_dict( dictionary, key, value):
+    if key in dictionary:
+        dictionary[key].append(value)
+    else:
+        dictionary[key] = [value]
+
+     
 
 
 prepreocessing()

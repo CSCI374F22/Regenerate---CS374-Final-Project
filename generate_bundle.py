e753beafc7e82cdb368d5537ca2f3a9b9e983395
root <root@LAPTOP-0RH61OUE.localdomain>
2022-12-12 23:29:35 -0500

    yaga

--- generate_bundle.py
+++ generate_bundle.py
@@ -14,18 +14,31 @@ from note_seq import chords_encoder_decoder
 from magenta.pipelines import note_sequence_pipelines
 from magenta.models.shared import sequence_generator
 
-def generate_bundle():
-    gen_details = generator_pb2.GeneratorDetails(
+checkpoint_file = 'training_checkpoints'
+gen_details = generator_pb2.GeneratorDetails(
                 id='polyphony_rnn',
                 description='regenerate.')
-    base_model = model.BaseModel()
-    base_gen = sequence_generator.BaseSequenceGenerator(
-        model= base_model,
-        details = gen_details,
-        checkpoint='training_checkpoints',
-        bundle = None)
-    base_gen.create_bundle_file('bundle_path',None)
 
+def generate_bundle():
+
+    base_gen = sequence_generator.BaseSequenceGenerator(
+        model= model.BaseModel,
+        details = gen_details,
+        checkpoint = checkpoint_file,
+        bundle = None)
+
+    print("base_gen is type:",type(base_gen))
+
+    poly_gen = polyphony_sequence_generator.PolyphonyRnnSequenceGenerator(
+        model = base_gen,
+        details = gen_details,
+        checkpoint = checkpoint_file, 
+        bundle = None)
+
+    print("poly_gen is type:",poly_gen)
+
+
+    poly_gen.create_bundle_file('bundle_path',None)
 
 def main():
     generate_bundle()
